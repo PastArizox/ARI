0993ba6b01a62601618500198c4931469d72d648
PastArizox <arizoxfr@gmail.com>
2023-08-29 13:46:13 +0200

    Added avatar command

---
+++ src/commands/miscellaneous/avatar.ts
@@ -0,0 +1,41 @@
+import {
+    CacheType,
+    Colors,
+    CommandInteraction,
+    GuildMember,
+    SlashCommandBuilder,
+} from 'discord.js';
+import { SlashCommand } from '../../types';
+import { EmbedBuilder } from '@discordjs/builders';
+
+export const command: SlashCommand = {
+    name: 'avatar',
+    data: new SlashCommandBuilder()
+        .setName('avatar')
+        .setDescription('Get the avatar of a user')
+        .addUserOption((option) =>
+            option
+                .setName('user')
+                .setDescription('The user you want to get the avatar')
+                .setRequired(false)
+        )
+        .setDMPermission(false),
+    async execute(interaction: CommandInteraction<CacheType>) {
+        let member = interaction.options.get('user')?.member as GuildMember;
+        if (!member) member = interaction.member as GuildMember;
+        let user = member.user;
+
+        const username = user.username;
+        const avatar = user.avatarURL();
+
+        const embed = new EmbedBuilder()
+            .setTitle(`${username}'s avatar`)
+            .setDescription(
+                avatar ? null : "This user doesn't have any avatar."
+            )
+            .setImage(avatar)
+            .setColor(Colors.Yellow);
+
+        await interaction.reply({ embeds: [embed] });
+    },
+};
