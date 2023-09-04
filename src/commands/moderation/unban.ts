4aeecd756d2da6541074c928f0843e8c7ddacba2
PastArizox <arizoxfr@gmail.com>
2023-09-04 17:11:34 +0200

    Added unban command

---
+++ src/commands/moderation/unban.ts
@@ -0,0 +1,69 @@
+import {
+    CommandInteraction,
+    CacheType,
+    SlashCommandBuilder,
+    Colors,
+    User,
+    Guild,
+} from 'discord.js';
+import { SlashCommand } from '../../types';
+import { EmbedBuilder } from '@discordjs/builders';
+import { LogLevel, Logger } from '../../utils/logger';
+
+export const command: SlashCommand = {
+    name: 'unban',
+    data: new SlashCommandBuilder()
+        .setName('unban')
+        .setDescription('Unban a user from the server')
+        .addStringOption((option) =>
+            option
+                .setName('user_id')
+                .setDescription('The id of the user you want to unban')
+                .setRequired(true)
+        )
+        .addStringOption((option) =>
+            option
+                .setName('reason')
+                .setDescription('The reason why you want to unban the user')
+                .setRequired(false)
+        ),
+    async execute(interaction: CommandInteraction<CacheType>) {
+        let userId = interaction.options.get('user_id')?.value as string;
+
+        let reason = interaction.options.get('reason')?.value as string;
+        if (!reason) reason = 'Unknown';
+
+        let description: string;
+        let passed = false;
+
+        try {
+            await interaction.guild?.members.unban(userId);
+            passed = true;
+            description = `✅ **${userId}** has been unbanned from the server`;
+        } catch {
+            description = '❌ This user is not banned from this server';
+        }
+
+        const embed = new EmbedBuilder()
+            .setTitle(description)
+            .setImage(
+                passed
+                    ? 'https://media.tenor.com/4CPKscdlZEAAAAAC/eric-young-jail.gif'
+                    : null
+            )
+            .setColor(Colors.Green);
+
+        interaction.reply({ embeds: [embed] });
+
+        if (passed) {
+            Logger.log(
+                interaction.guild as Guild,
+                '✅ User unbanned',
+                interaction.member?.user as User,
+                reason,
+                LogLevel.INFO,
+                [{ title: 'Unbanned UserId', value: userId }]
+            );
+        }
+    },
+};
